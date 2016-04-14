2bc87cf8d626dd9ac111aecc089d3cf8775d1c45
Shih-Po <spurcheng@gmail.com>
2016-04-14 15:10:27 +0800

    change all the structure

---
+++ offensive_db/functions/check_function.R
@@ -0,0 +1,10 @@
+# 需要人工校閱的條件, to_check <- 1
+check_function = function(dummy_list, log_row) {
+  
+  # length greater than 40  
+  if (nchar(log_row) > 40) {
+    dummy_list$to_check <- 1
+  }
+  
+  return(dummy_list)
+}
