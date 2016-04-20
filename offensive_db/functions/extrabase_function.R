7f27519754b78501a1a48cc7bd1d413ec5c8ceb1
Eric Chu <eric.chu1223@gmail.com>
2016-04-20 15:43:16 +0800

    casual update
    improvement on model

--- offensive_db/functions/extrabase_function.R
+++ offensive_db/functions/extrabase_function.R
@@ -1,4 +1,4 @@
-
+if (grepl("一分",log_row)){
     if (base3 != "NA"){
     base3 = "NA"
 
@@ -6,11 +6,14 @@
 
         if (base2 != "NA") {
           base2 = "NA"
-        } else if(base2 == "NA"){
-            if (base1 != "NA") {
+          
+        }
+      } else if(base2 == "NA"){
+            
+          if (base1 != "NA") {
               base1 = "NA"
               }
-        }
-
-    }
+      }
+  rem_type <- rem_type
+}
 #當跑者積極跑壘造成得分，從預設情形中，移出最靠近本壘之跑者。
