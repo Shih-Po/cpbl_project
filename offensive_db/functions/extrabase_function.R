b18e016d70123f6afb99b3d493ed846fd9c303ff
Eric Chu <eric.chu1223@gmail.com>
2016-04-20 15:02:01 +0800

    update on extrabase_function
    在想到更好的方法之前暫時只能醜醜的放在hit_function裡面...

--- offensive_db/functions/extrabase_function.R
+++ offensive_db/functions/extrabase_function.R
@@ -1,33 +1,16 @@
-extrabase_function = function(dummy_list){
-    
-  rem_type <- dummy_list$rem_type
-  base1 <- dummy_list$base1
-  base2 <- dummy_list$base2
-  base3 <- dummy_list$base3
-  player <- dummy_list$player
-  
+
     if (base3 != "NA"){
     base3 = "NA"
-          
-    }else if(base3 == "NA"){
-        
-        if (base2 != "NA"){
-        base2 = "NA"
-        
-        }else if(base2 == "NA"){
-            
-            if (base1 != "NA"){
-            base1 = "NA"
-                                
-            }
-        }  
-    }
-  
-  dummy_list$rem_type <- rem_type
-  dummy_list$base1 <- base1
-  dummy_list$base2 <- base2
-  dummy_list$base3 <- base3 
-  dummy_list$player <- player
-  return (dummy_list);
-}    
 
+    } else if(base3 == "NA"){
+
+        if (base2 != "NA") {
+          base2 = "NA"
+        } else if(base2 == "NA"){
+            if (base1 != "NA") {
+              base1 = "NA"
+              }
+        }
+
+    }
+#當跑者積極跑壘造成得分，從預設情形中，移出最靠近本壘之跑者。
