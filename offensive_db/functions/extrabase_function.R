fff4518798de85545735b7edb410ff71638605a6
Eric Chu <eric.chu1223@gmail.com>
2016-04-20 11:54:58 +0800

    update extrabase_function
    展開與輸出

--- offensive_db/functions/extrabase_function.R
+++ offensive_db/functions/extrabase_function.R
@@ -1,5 +1,11 @@
-extrabase_function = function(dummy_list, log_row){
-        
+extrabase_function = function(dummy_list){
+    
+  rem_type <- dummy_list$rem_type
+  base1 <- dummy_list$base1
+  base2 <- dummy_list$base2
+  base3 <- dummy_list$base3
+  player <- dummy_list$player
+  
     if (base3 != "NA"){
     base3 = "NA"
           
@@ -16,6 +22,12 @@ extrabase_function = function(dummy_list, log_row){
             }
         }  
     }
+  
+  dummy_list$rem_type <- rem_type
+  dummy_list$base1 <- base1
+  dummy_list$base2 <- base2
+  dummy_list$base3 <- base3 
+  dummy_list$player <- player
   return (dummy_list);
 }    
 
