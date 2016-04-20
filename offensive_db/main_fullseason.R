4e43cce2bac6064e5a22168e5ca1f7e29fd07cf4
Shih-Po Cheng <spurcheng@gmail.com>
2016-04-20 11:04:49 +0800

    Add source functions

--- offensive_db/main_fullseason.R
+++ offensive_db/main_fullseason.R
@@ -2,7 +2,8 @@ library(dplyr)
 # 1. source all functions
 # remember add encoding = "UTF-8"
 source_all = function() {
-  c_function_name <- c("check", "hit1", "hit2", "hit3", "homerun", "inning", "outs", "strikeout", "walk")
+  c_function_name <- c("check", "hit1", "hit2", "hit3", "homerun", "inning", "outs", "strikeout", "walk",
+                       "nomalize_log", "extrabase")
   for (i in 1:length(c_function_name)) {
     # function_path <- paste0("/Users/shipo/Documents/cpbl_project/offensive_db/functions/", c_function_name[i], "_function.R")
     function_path <- paste0("D:/cpbl_project/offensive_db/functions/", c_function_name[i], "_function.R")
