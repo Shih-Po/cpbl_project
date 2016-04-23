1104868da763979c2f0a029c7b553cf7e2713dda
Shih-Po <spurcheng@gmail.com>
2016-04-23 15:28:32 +0800

    REM use 042203.csv
    這次的數字看起來是有模有樣了，1B_3B期望值較低可能和to_check還沒處理有關，如果要使用rem_operator的話改一下檔案路徑就好，
    記得改設定重算前要先清除R的變數

--- offensive_db/tools/rem_operator.R
+++ offensive_db/tools/rem_operator.R
@@ -1,25 +1,29 @@
 library(dplyr)
-off_db_file <- "/Users/shipo/Documents/cpbl_project/temp/output042100_utf8.csv" 
+off_db_file <- "/Users/shipo/Documents/cpbl_project/temp/output042203_utf8.csv" 
 off_db <- read.csv(off_db_file)
 
 # 找壘包出局情境為 1~24, 且後續得分為 0~100 的資料列
 odb_rtype <- off_db %>% filter(rem_type %in% c(1:24), follow.up %in% c(0:100))
 
 # B. 找球隊 (客隊上半局為攻擊、主隊下半局為攻擊)
-# or_away <- odb_rtype %>% filter(grepl(pattern = "兄弟", x = away, fixed = TRUE), grepl(pattern = "上", x = inning, fixed = TRUE)) 
-# or_home <- odb_rtype %>% filter(grepl(pattern = "兄弟", x = home, fixed = TRUE), grepl(pattern = "下", x = inning, fixed = TRUE)) 
-# odb_rtype <- rbind(odb_rtype_away, odb_rtype_home)
+# or_away <- odb_rtype %>% filter(grepl(pattern = "統一", x = away, fixed = TRUE), grepl(pattern = "上", x = inning, fixed = TRUE)) 
+# or_home <- odb_rtype %>% filter(grepl(pattern = "統一", x = home, fixed = TRUE), grepl(pattern = "下", x = inning, fixed = TRUE)) 
+# odb_rtype <- rbind(or_away, or_home)
 
 # C. 找球員
 # odb_rtype <- odb_rtype %>% filter(Player == "胡金龍")
-re_matrix <- list()
+re_list <- list()
+
+
 
 # 計算24種 rem_type 的得分期望值
 for (i in 1:24) {
   odb_r <- odb_rtype %>% filter(rem_type == i)
   r <- sum(odb_r$follow.up) / nrow(odb_r)
-  re_matrix[i] <- r
+  re_list[i] <- r
 }
-
-# output a list
+# output a martix, and change col, row names
+re_matrix <- matrix(re_list, nrow = 8, byrow=T)
+colnames(re_matrix) <- c("out0", "out1", "out2")
+rownames(re_matrix) <- c("empty", "1B", "2B", "3B", "1B_2B", "1B_3B", "2B_3B", "1B_2B_3B")
 re_matrix
